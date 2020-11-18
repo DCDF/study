07ceacf08986f48d8469b557941c332ec453ead7
dcdf <935322337@qq.com>
2020-11-18 15:33:42 +0800

    工具方法:获取本月第一个周一的LocalDateTime对象

---
+++ Hammer/src/utils/HammerTime.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.time.LocalDateTime;
+
+public class HammerTime {
+
+	public static LocalDateTime getFirstZhouYiDayOfMonth() {
+		LocalDateTime time = LocalDateTime.now();
+		LocalDateTime monthFirstDay = LocalDateTime.of(time.getYear(), time.getMonth(), 1, 0, 0);
+		int value = monthFirstDay.getDayOfWeek().getValue();
+		if (value != 1) {// 本月第一天不是新的一周开始的
+			int over = 8 - value;
+			monthFirstDay = LocalDateTime.of(time.getYear(), time.getMonth(), 1 + over, 0, 0);
+		}
+		return monthFirstDay;
+	}
+}
